7067395c9859bbd50847ad73ca758334161a7863
stDem <61629062+stDem@users.noreply.github.com>
2023-09-09 13:43:10 +0300

    finished Homework_5

---
+++ s_5_Homework/s_5_data.js
@@ -0,0 +1,20 @@
+export const catFacts = `[
+    {
+        "id": "1",
+        "number": "70%",
+        "fact": "Cats spend 70% of their lives sleeping",
+        "url": "https://www.purina.co.uk/sites/default/files/styles/ttt_image_690/public/2020-12/14%20Fun%20Facts%20about%20Cats1.webp?itok=leX341so"
+    },
+    {
+        "id": "2",
+        "number": "95.6%",
+        "fact": "House cats share 95.6% of their genetic makeup with tigers",
+        "url": "https://www.purina.co.uk/sites/default/files/styles/ttt_image_690/public/2020-12/14%20Fun%20Facts%20about%20Cats3.webp?itok=JHapFW09"
+    },
+    {
+        "id": "3",
+        "number": "100%",
+        "fact": "Cats walk like camels and giraffes",
+        "url": "https://www.purina.co.uk/sites/default/files/styles/ttt_image_690/public/2020-12/14%20Fun%20Facts%20about%20Cats2.webp?itok=YE_e-LI3"
+    }]
+    `;
